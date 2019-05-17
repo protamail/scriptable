31312130b5411498c26f6d8ed0c70109c9935922
Alex Protasenko <aprotasenko@bkmks.com>
2019-05-17 18:37:09 -0400

    check

--- setup-new-project.sh
+++ setup-new-project.sh
@@ -12,7 +12,8 @@ mkdir "../$1" && cd "../$1" && git init &&
     git clone https://github.com/protasenko/scriptable-deps.git lib/scriptable-deps/ &&
     # NOTE: trailing slash is important here, or git won't add cloned sub-repository
     git add lib/scriptable-deps/ &&
-    cp -fr "$SDIR/sample/"* . &&
+    # NOTE: trailing . will include all dot files as well
+    cp -fr "$SDIR/sample/." . &&
     mv "$SDIR/../scriptable" src/ &&
     git add src/scriptable/ &&
     git add . && git commit -a -m "Initial"
